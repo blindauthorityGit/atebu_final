444f5f656f0eba724c3aebbaa8e30803c9812848
Johannes <office@atelierbuchner.at>
2023-02-17 15:23:27 +0100

    tablet

--- components/container/containervh100.jsx
+++ components/container/containervh100.jsx
@@ -4,7 +4,7 @@ import HugeLogo from "../../assets/hugeLogo.svg";
 const ContainerVH100 = (props) => {
     return (
         <div
-            className={`containervh100 sm:min-h-screen relative overflow-hidden ${
+            className={`containervh100 lg:min-h-screen relative overflow-hidden ${
                 props.center ? "items-center flex" : ""
             }  ${props.hFull ? "h-full" : ""} ${props.first ? "py-16 sm:py-24" : "py-8 sm:py-24"}  w-full ${
                 props.klasse
