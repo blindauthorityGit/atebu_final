ee7afbd77164c29f0ca593c7d4a2395c68bd707c
Johannes <office@atelierbuchner.at>
2023-02-17 13:58:25 +0100

    tablet

--- components/mobileBar/mobileBar1.jsx
+++ components/mobileBar/mobileBar1.jsx
@@ -18,7 +18,7 @@ const MobileBar = () => {
                 <span className="text-xs font-base mt-1">EMAIL</span>
             </a>
             <a
-                href="https://api.whatsapp.com/send?phone=+436509444150"
+                href="https://wa.link/x69iqk"
                 className="w-1/3 p-4 flex flex-col justify-center items-center text-[#6DC593] hover:text-green-600"
             >
                 <FaWhatsapp size={18} />
