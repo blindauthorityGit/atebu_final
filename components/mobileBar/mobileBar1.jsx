b33b9a159b4e593c318dac3f1b9d5a8ff0583c7b
Johannes <office@atelierbuchner.at>
2023-02-17 13:37:45 +0100

    tablet

--- components/mobileBar/mobileBar1.jsx
+++ components/mobileBar/mobileBar1.jsx
@@ -2,16 +2,16 @@ import { FaPhone, FaEnvelope, FaWhatsapp } from "react-icons/fa";
 
 const MobileBar = () => {
     return (
-        <div className="fixed sm:hidden z-40 bottom-0 w-full flex justify-center items-center bg-white shadow-md">
-            <button className="w-1/3 p-4 flex flex-col justify-center items-center text-gray-600 hover:text-gray-800">
+        <div className="fixed sm:hidden  z-40 bottom-0 w-full flex justify-center items-center bg-blackText shadow-md">
+            <button className="w-1/3 p-4 flex flex-col justify-center items-center text-primaryColor hover:text-gray-800">
                 <FaPhone size={24} />
                 <span className="text-sm font-medium">CALL</span>
             </button>
-            <button className="w-1/3 p-4 flex flex-col justify-center items-center text-gray-600 hover:text-gray-800">
+            <button className="w-1/3 p-4 flex flex-col justify-center items-center text-primaryColor hover:text-gray-800">
                 <FaEnvelope size={24} />
                 <span className="text-sm font-medium">EMAIL</span>
             </button>
-            <button className="w-1/3 p-4 flex flex-col justify-center items-center text-green-500 hover:text-green-600">
+            <button className="w-1/3 p-4 flex flex-col justify-center items-center text-[#6DC593] hover:text-green-600">
                 <FaWhatsapp size={24} />
                 <span className="text-sm font-medium">WHATSAPP</span>
             </button>
