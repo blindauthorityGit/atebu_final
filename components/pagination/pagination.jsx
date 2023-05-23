a6a99941a01bd866ef7558bf59921eb0d667999c
Johannes <office@atelierbuchner.at>
2023-05-23 17:44:34 +0200

    heoHeight & pagination

--- components/pagination/pagination.jsx
+++ components/pagination/pagination.jsx
@@ -4,9 +4,11 @@ const Pagination1 = () => {
     return (
         <div className="w-full flex justify-center">
             <div className="flex w-24 justify-evenly items-center">
+                <div className="circle w-1 h-1 rounded-full bg-white opacity-50"></div>
                 <div className="circle w-2 h-2 rounded-full bg-white opacity-50"></div>
-                <div className="circle w-4 h-4 rounded-full bg-primaryColor-100"></div>
+                <div className="circle w-3 h-3 rounded-full bg-primaryColor-100"></div>
                 <div className="circle w-2 h-2 rounded-full bg-white opacity-50"></div>
+                <div className="circle w-1 h-1 rounded-full bg-white opacity-50"></div>
             </div>
         </div>
     );
